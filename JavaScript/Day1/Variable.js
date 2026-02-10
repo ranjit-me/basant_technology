88b7b0e25d1a01fe47217b6db5ee59fa0397134e
Ranjit <ranjitbichukale11@gmail.com>
2026-02-10 09:34:17 +0530

    Javascript basic and variable

---
+++ JavaScript/Day1/Variable.js
@@ -0,0 +1,29 @@
+/*
+    variable is used to store data
+    var
+    let
+    const
+    variables can be reintialsed and declerd
+*/
+// var num = 12;
+// var num2 = 9;
+// var name = "Ranjit";
+// var sum = num + num2;
+// name = "sham";
+// console.log("My name :-" + name + "   " + "Age:-" + num);
+
+/*
+    let learning
+    let can not be reinital but decleard
+*/
+// let name = "David";
+// name = "Jhon Snow";
+// console.log("my name :- ", name);
+
+/* 
+    const learning
+    we can not reinital or declear again
+*/
+const name = "Ranjit";
+// name = "Johan Snow";
+console.log(name);
