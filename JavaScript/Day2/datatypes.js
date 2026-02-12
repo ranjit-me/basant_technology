1ee7668eed367c0e4463932664006280888f9b06
Ranjit <ranjitbichukale11@gmail.com>
2026-02-12 09:10:37 +0530

    operator started

--- JavaScript/Day2/datatypes.js
+++ JavaScript/Day2/datatypes.js
@@ -1,9 +1,11 @@
-console.log(typeof 123);
-console.log(typeof "Rohit");
-console.log(typeof 1.2);
-console.log(typeof null);
-console.log(typeof undefined);
-console.log(typeof false);
+// console.log(typeof 123);
+// console.log(typeof "Rohit");
+// console.log(typeof 1.2);
+// console.log(typeof null);
+// console.log(typeof undefined);
+// console.log(typeof false);
 
-let name;
-console.log("name value", name);
+// let name;
+// console.log("name value", name);
+
+console.log("Hello");
---
+++ JavaScript/Day2/operator.js
@@ -0,0 +1,45 @@
+/* 
+    Operator
+        1)Arithmatic Operator
+            a)Add(+)
+            b)sub(-)
+            c)mul(*)
+            d)div(/)
+            e)mod(%)
+            f)postincrement(a++)
+            g)preincriment(--a)
+            h)power(a**2)
+        2)Assignment Oprtor
+        
+                 i)i=i+10.  or i+=10;
+                ii)i=i*10.  or i*=10;
+                iii)i=i/10.  or i/=10;
+                iv)i=i%10.   or i%=2;
+                v)i=i**3.    or i**=2;
+        3)Conditional Operator
+            a)  ==
+            b)  !=
+            c)  >=
+            d). <=
+            e)  !==
+            f)  ===
+        4)Relational Operator
+
+        5)ternary Oprator 
+*/
+
+console.log(5 + 3);
+console.log(5 - 3);
+console.log(5 * 3);
+console.log(5 / 3);
+console.log(5 % 3);
+console.log(5 ** 2);
+let c = 10;
+console.log(--c);
+let d = c++;
+console.log(d++);
+
+let i = 10;
+console.log((i += 10));
+console.log("5" == 5);
+console.log("5" === 5);
