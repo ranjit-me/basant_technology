3674b783d04d16098c73757757b92acc5f514706
Ranjit <ranjitbichukale11@gmail.com>
2026-02-12 09:36:40 +0530

    conditianl complited

---
+++ JavaScript/Day2/condition.js
@@ -0,0 +1,15 @@
+let age = 10;
+if (age > 18) {
+  console.log("Eligible to get DL");
+} else {
+  console.log("Not Eligible");
+}
+
+let teamname = "RCB";
+if (teamname === "RCB") {
+  console.log("Es sala cup namde");
+} else if (teamname === "CSK") {
+  console.log("We have so much in list");
+} else {
+  console.log("Still Wating....");
+}
