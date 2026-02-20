471657dada7ae666a593ea842e194e4a421c7687
Ranjit <ranjitbichukale11@gmail.com>
2026-02-20 08:55:43 +0530

    19

--- JavaScript/Day4/Templateliterals.js
+++ JavaScript/Day4/Templateliterals.js
@@ -10,29 +10,4 @@ function templateLiterals() {
     _______
     ${name}`);
 }
-function forInLoop() {
-  // for in loop
-  // for in loop generally used to iterate there object
-  let obj = {
-    name: "David",
-    Number: "123",
-  };
-  for (let key in obj) {
-    console.log(`key:${key}`);
-    //   console.log(key);
-    console.log(obj[key]);
-  }
-}
-function forOfLoop() {
-  //for of loop used to iterate both
-  let arr = ["1", "2", "3", "4", "5"];
-  for (let value of arr) {
-    console.log(value);
-  }
-  let city = "Bangalore";
-  for (let char of city) {
-    console.log(char);
-  }
-}
 
-forOfLoop();
